f7ca3c271a337e40912e65a0c0e8e01f6780c5cd
Chang <1948566272@qq.com>
2022-08-19 20:24:39 +0800

    second commit

--- src/main/java/com/chang/git/GitTest.java
+++ src/main/java/com/chang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.chang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
